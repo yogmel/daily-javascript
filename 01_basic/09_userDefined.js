05102b997fb15605b26d168a3dc4bade67cf1cc0
yogmel <pong7219@gmail.com>
2018-11-27 21:20:10 -0200

    Exercise 13 - better regexp

--- 01_basic/09_userDefined.js
+++ 01_basic/09_userDefined.js
@@ -1,13 +1,21 @@
 (function() {
   const userVariableInput = document.getElementById('userVariable')
   const userForm = document.getElementById('userForm')
+  let output = document.getElementById('output')
+  let warning = document.getElementById('warning')
 
-  console.log(userVariableInput)
-
-  userForm.addEventListener('click', (e) => {
+  userForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    const regex1 = /(\w+)?(\d+)?(\$)?/
-    console.log(regex1)
+    const value = userVariableInput.value
+    const regex1 = /^[\D|\$|\_](\S+)$/
+
+    if(regex1.test(value)) {
+      warning.innerHTML = ''
+      userVariableInput.value = ''
+      return output.innerHTML += '<p>' + value + '</p>'
+    } else {
+      warning.innerHTML = 'Variáveis não podem iniciar com números e não podem haver espaços.'
+    }
   })
 
 })()
